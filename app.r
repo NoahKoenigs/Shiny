a7df59fe1fcb09ef3299f63229ec5330ac7087cc
Noah Koenigs <koenigs.5@buckeyemail.osu.edu>
2023-07-26 01:54:28 +0000

    update cannot run shiny

--- app.r
+++ app.r
@@ -42,11 +42,24 @@ ui <- fluidPage(
             tags$hr(),
             # Input: Select number of rows to display ----
             radioButtons("disp", "Display",
-                         choices = c(Head = "head",
-                                     All = "all"),
-                         selected = "all")
+                         choices = c(Head = "Preview",
+                                     None = "None",
+                                     All = "All"),
+                         selected = "None")
+            #Adding Action Button
+            actionButton("btn", "Click Me")
         ),
-        # Show a plot of the generated distribution#######################################################################################
+            tags$head(tags$style(HTML("
+                #dem {
+                    title: "text/csv";
+                    width:150px;
+                    height:60px;
+                    position: relative;
+                }
+                ")))
+            tags$div(id= "dem", "Initial Text")
+        ),
+        # Show a plot of the generated distribution
         mainPanel(
            plotOutput("distPlot"),
            plotOutput("scatter"),
@@ -65,16 +78,14 @@ server <- function(input, output) {
                        quote = input$quote)
         return(df)
     })
-    # output$distPlot <- renderPlot({
-    #     # generate bins based on input$bins from ui.R
-    #     x    <- faithful[, 2]
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    #     print(bins)
-    #     # draw the histogram with the specified number of bins
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    # })
-    #  
+    #Observe Tag
+    observeEvent(input$btn, {
+        updateTextInput(session, "dem", "Text Updated")
+    })
     output$distPlot <- renderPlot({
+        plot(dataInput()$x,dataInput()$y)
+    })
+    output$scatter <- renderPlot({
         plot(dataInput()$x,dataInput()$y)
     })
     output$lmtPlot <- renderPlot({
@@ -84,11 +95,12 @@ server <- function(input, output) {
         # input$file1 will be NULL initially. After the user selects
         # and uploads a file, head of that data file by default,
         # or all rows if selected, will be shown.
-        if(input$disp == "head") {
+        if(input$disp == "Preview") {
             return(head(dataInput()))
-        }
-        else {
+        } else if (input$disp =="All") {
             return(dataInput())
+        } else {
+            return(NULL)
         }   
     })       
 }
